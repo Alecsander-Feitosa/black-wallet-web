b709e57911d6a19102771d39d78cca49ec01a3dc
Alecsander-Feitosa <Alecsandersfr@gmail.com>
2026-03-10 07:08:10 -0300

    Import time module in app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from web3 import Web3
+import time
 cached_prices = None
 last_update = 0
 
